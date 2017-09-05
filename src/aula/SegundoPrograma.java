ef5b0fa94e6eda42c6e1c4f4869d9091175dc039
Aluno_2 <Aluno_2@LAB-06-20>
2017-09-05 19:51:56 -0300

    Calculadora com funções básicas da aritimética

---
+++ src/aula/SegundoPrograma.java
@@ -0,0 +1,11 @@
+package aula;
+
+public class SegundoPrograma {
+	
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		// Teste de publicação no gitHub
+		System.out.println("publicando o primeiro programa no gitHub");
+	}
+	
+}
